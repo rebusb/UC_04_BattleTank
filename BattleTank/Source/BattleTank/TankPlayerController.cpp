2072a0eda94fb74808c2ff2b1f295c1d8910407a
rebus <rebus@rebusstudios.com>
2019-01-29 14:10:46 -0500

    L134 - Tick() override added, play with PlayerTick() and PrimaryActorTick.bCanEverTick - precode AimTowardCrosshair()

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,14 +3,54 @@
 
 #include "TankPlayerController.h"
 #include "BattleTank.h"
+#include "GameFramework/GameMode.h"
 
+//experiment to see if can kill tick...
+ATankPlayerController::ATankPlayerController(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
+{
+	//setting this to false does kill the tick()
+	PrimaryActorTick.bCanEverTick = true;
+}
 
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("ATankPlayerController %s BeginPlay()"), *(GetName()));
+
+	bHasTicked = false; 
+	bHasPlayerTicked = false;
+
+	SetActorTickEnabled(true);
+
 	const ATank *Temp = GetControlledTank();
 }
+
+
+//void ATankPlayerController::PlayerTick(float DeltaTime)
+//{
+//
+//	Super::PlayerTick(DeltaTime);
+//
+//	if (!bHasPlayerTicked)
+//	{
+//		UE_LOG(LogTemp, Warning, TEXT("ATankPlayerController has PlayerTicked()."));
+//		bHasPlayerTicked = true;
+//	}
+//
+//}
+
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!bHasTicked)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ATankPlayerController has ticked."));
+		bHasTicked = true;
+	}
+}
+
 
 
 ATank* ATankPlayerController::GetControlledTank() const
@@ -29,6 +69,15 @@ ATank* ATankPlayerController::GetControlledTank() const
 
 }
 
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) {return;}
+	
+	// Get world location of linetrace through crosshair
+	// if it gets hit (landscape, enemy?) 
+		// tell controlled tank to aim at this point
+}
 
 
 
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -17,9 +17,23 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+protected:
+	// flag to check if tick() has fired to prevent log spamming
+	bool bHasTicked;
+	bool bHasPlayerTicked;
+
 public:
+	ATankPlayerController(const FObjectInitializer& ObjectInitializer);
+
 	ATank* GetControlledTank() const;
 	
 	void BeginPlay() override;
+
+	void Tick(float DeltaTime) override;
+	//	void PlayerTick(float DeltaTime) override;
+
+private:
+	//Start moving barrel toward aim crosshair
+	void AimTowardsCrosshair();
 
 };
