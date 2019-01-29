f8891394276f7f52f065350338c3e8b410b8a7c3
rebus <rebus@rebusstudios.com>
2019-01-28 23:14:27 -0500

    L132 - Tank AI Controller

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,19 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "TankPlayerController.h"
-#include "BattleTank.h"
+#include "TankAIController.h"
 
 
-void ATankPlayerController::BeginPlay()
+
+
+//#include "BattleTank.h"
+
+
+void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("ATankPlayerController %s BeginPlay()"), *(GetName()));
+	UE_LOG(LogTemp, Warning, TEXT("TankAIController %s BeginPlay()"),*(GetName()));
 	const ATank *Temp = GetControlledTank();
 }
 
 
-ATank* ATankPlayerController::GetControlledTank() const
+ATank* ATankAIController::GetControlledTank() const
 {
 
 	ATank* TankPawn = Cast<ATank>(GetPawn());
@@ -28,7 +31,3 @@ ATank* ATankPlayerController::GetControlledTank() const
 	return TankPawn;
 
 }
-
-
-
-
