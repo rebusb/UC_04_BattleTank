cfc95a03554559306599aad03793e2c816db5a3f
rebus <rebus@rebusstudios.com>
2019-01-29 00:10:52 -0500

    L133 - Get Player Controller (in AI Controller)

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
-
+#include "Engine/World.h"
 
 
 
@@ -12,7 +12,18 @@ void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("TankAIController %s BeginPlay()"),*(GetName()));
-	const ATank *Temp = GetControlledTank();
+	ATank *OurTank = GetControlledTank();
+	ATank *PlayerTank = GetPlayerTank();
+
+	if (PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Got Player Tank %s"), *PlayerTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Did NOT get Player Tank !!!"));
+	}
+
 }
 
 
@@ -30,4 +41,11 @@ ATank* ATankAIController::GetControlledTank() const
 	}
 	return TankPawn;
 
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (!PlayerController) { return nullptr; }
+	return  Cast<ATank>(PlayerController->GetPawn());
 }
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -18,9 +18,13 @@ class BATTLETANK_API ATankAIController : public AAIController
 	GENERATED_BODY()
 	
 public:
-	ATank * GetControlledTank() const;
-
 	void BeginPlay() override;
+
+private:
+
+	ATank* GetControlledTank() const;
+	ATank* GetPlayerTank() const;
+
 
 	
 	
