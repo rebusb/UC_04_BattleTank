bd5d74dcd20ad21c2601d86889f215c1652bb79f
rebus <rebus@rebusstudios.com>
2019-02-04 15:06:21 -0500

    First cherry pick

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -6,21 +6,13 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-
-	
-	
 	if (Throttle!=0.0)
 	{
 		auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 		auto ForceLocation = GetComponentLocation();
 		auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 		TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
-		auto Name = GetName();
-		UE_LOG(LogTemp, Warning, TEXT("%s Applies \tforce %s \tat %s"),
-			*Name, *ForceApplied.ToString(), *ForceLocation.ToString());
-
 	}
-
 }
 
 
