76566c5b2723b8bd41244e736a7297464fa781bc
rebus <rebus@rebusstudios.com>
2019-02-06 18:22:06 -0500

    L172 - CrossProduct() - AITanks turn...

--- BattleTank/Source/BattleTank/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/TankMovementComponent.h
@@ -26,11 +26,11 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 		void InitializeTreads(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 	
-	//override NavMovementComp' method
-	void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
-
 private:
 	UTankTrack* TreadRight = nullptr;
 	UTankTrack* TreadLeft = nullptr;
+
+	//override NavMovementComp' method
+	void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 
 };
